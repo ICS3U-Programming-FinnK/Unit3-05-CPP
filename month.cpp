9e24e4b29fbecce03f9443c60dfdb966835b8e3b
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2023-10-20 17:00:54 +0000

    Updated month.cpp

--- month.cpp
+++ month.cpp
@@ -1,33 +1,70 @@
 // # Created by: Finn Kitor
-// Created on : October 18th, 2023
+// Created on : October 20th, 2023
 // this program asks the user for the month as a number between 1 to 12.
 
 #include <iostream>
-#include <map>
-#include <string>
-// the terminal will return the month in the string format
-std::string
-find_month(int month) {
-    std::map<int, std::string> months = {
-        {1, std::to_string(month) + " represents january."},
-        {2, std::to_string(month) + " represents february."},
-        {3, std::to_string(month) + " represents march."},
-        {4, std::to_string(month) + " represents april."},
-        {5, std::to_string(month) + " represents may."},
-        {6, std::to_string(month) + " represents june."},
-        {7, std::to_string(month) + " represents july."},
-        {8, std::to_string(month) + " represents august."},
-        {9, std::to_string(month) + " represents september."},
-        {10, std::to_string(month) + " represents october."},
-        {11, std::to_string(month) + " represents november."},
-        {12, std::to_string(month) + " represents december."}};
-    return months[month];
-}
-// the user will input the month here
+
+
 int main() {
-    int user_month;
-    std::cout << "Enter the number that represents the month: ";
-    std::cin >> user_month;
-    std::cout << find_month(user_month) << std::endl;
-    return 0;
+    //declare the months
+    int userMonth;
+
+    //the terminal will ask the month from 1-12
+    std::cout << "enter the month from 1-12: ";
+    std::cin >> userMonth;
+    
+    // the month that is selected by a string
+    switch (userMonth) {
+        case 1 :
+            std::cout << userMonth << "is January.\n";
+            break;
+
+        case 2 :
+            std::cout << userMonth << "is February.\n";
+            break;
+
+        case 3 :
+            std::cout << userMonth << "is March.\n";
+            break;
+
+        case 4 :
+            std::cout << userMonth << "is April.\n";
+            break;
+
+        case 5 :
+            std::cout << userMonth << "is May.\n";
+            break;
+
+        case 6 :
+            std::cout << userMonth << "is June.\n";
+            break;
+
+        case 7 :
+            std::cout << userMonth << "is July.\n";
+            break;
+
+        case 8 :
+            std::cout << userMonth << "is August.\n";
+            break;
+
+        case 9 :
+            std::cout << userMonth << "is September.\n";
+            break;
+
+        case 10 :
+            std::cout << userMonth << "is October.\n";
+            break;
+
+        case 11 :
+            std::cout << userMonth << "is November.\n";
+            break;
+
+        case 12 :
+            std::cout << userMonth << "is December.\n";
+            break;
+
+            //responce for the errors
+            default :
+                std::cout << "Error." << userMonth << " Does not represent a month\n";
+    }
 }
